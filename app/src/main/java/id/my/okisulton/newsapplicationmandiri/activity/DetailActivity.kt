bd0deabbdd5dda1cd8160847571683012b1133d3
oexlye08 <os.alimie@gmail.com>
2022-08-04 11:36:55 +0700

    add loading in detail

--- app/src/main/java/id/my/okisulton/newsapplicationmandiri/activity/DetailActivity.kt
+++ app/src/main/java/id/my/okisulton/newsapplicationmandiri/activity/DetailActivity.kt
@@ -1,8 +1,12 @@
 package id.my.okisulton.newsapplicationmandiri.activity
 
+import android.annotation.SuppressLint
+import android.app.Dialog
 import android.os.Bundle
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import id.my.okisulton.newsapplicationmandiri.R
 import id.my.okisulton.newsapplicationmandiri.databinding.ActivityDetailBinding
 import id.my.okisulton.newsapplicationmandiri.network.model.NewsResponse
 import id.my.okisulton.newsapplicationmandiri.utils.Constants.INTENT_WITH_DATA
@@ -11,13 +15,19 @@ class DetailActivity : AppCompatActivity() {
 
     private var _binding : ActivityDetailBinding? = null
     private val binding get() = _binding!!
+    var progressDialog: Dialog? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        settingProgressDialog()
     }
 
     override fun onStart() {
+        progressDialog?.show()
         getUrl()
         super.onStart()
     }
@@ -28,9 +38,17 @@ class DetailActivity : AppCompatActivity() {
         initWebView(data.url)
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     private fun initWebView(url: String?) {
+        progressDialog?.show()
+
         binding.webView.apply {
-            webViewClient = WebViewClient()
+            webViewClient = object :WebViewClient() {
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    progressDialog?.dismiss()
+                }
+            }
             loadUrl(url.toString())
 
             val webSetting = settings
@@ -38,6 +56,14 @@ class DetailActivity : AppCompatActivity() {
             webSetting.javaScriptEnabled = true
             webSetting.domStorageEnabled = true
         }
+    }
 
+    private fun settingProgressDialog() {
+        progressDialog = Dialog(this)
+        progressDialog?.setContentView(R.layout.loading_animation)
+        progressDialog.let {
+            it?.setCancelable(false)
+            it?.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        }
     }
 }
